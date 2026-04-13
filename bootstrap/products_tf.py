6b779b7462bfcdca0ae28835710ef89a5c3abf13
Matias Gonzalez <matiastomasglez@gmail.com>
2026-04-13 15:44:10 -0300

    fix in continuos records, and modify distribution and probs of change

--- bootstrap/products_tf.py
+++ bootstrap/products_tf.py
@@ -1,44 +1,51 @@
 import pandas as pd
 import numpy as np
 
-orders_df = pd.read_csv("data/ecommerce_dataset/orders.csv")
 df = pd.read_csv("data/ecommerce_dataset/products.csv")
+orders_df = pd.read_csv("data/ecommerce_dataset/orders.csv")
+orders_df["order_date"] = pd.to_datetime(orders_df["order_date"])
 
-df["updated_at"] = True
+min_date = orders_df["order_date"].min()
+max_date = orders_df["order_date"].max()
 
-prob_of_change = 0.2
-max_changes = 1
+df["updated_at"] = min_date
+
+prob_of_change = 0.3
+max_changes = 2
 
 rows = []
 
 for _, row in df.iterrows():
-    price = row["price"]
-    rating = row["rating"]
-    
     rows.append(row.to_dict())
 
     if np.random.rand() < prob_of_change:
         new_row = row.copy()
+        n_changes = np.random.randint(1, max_changes + 1)
 
-        if np.random.rand() < 0.8:
-            price = row["price"]
-            price_update = np.random.normal(loc=0, scale=0.05)
-            new_row["price"] = np.round(price * (1 + price_update), 2)
-        else:
-            delta = np.random.normal(loc=0, scale=0.05)
-            new_rating = row["rating"] + delta
+        price = row["price"]
+        rating = row["rating"]
+        last_date = row["updated_at"]
+        
+        for _ in range(n_changes):
+            change_date = last_date + pd.Timedelta(
+                days=np.random.randint(5, 60),
+                seconds=np.random.randint(0, 86400),
+                microseconds=np.random.randint(0, 1_000_000))
 
-            new_rating = np.clip(new_rating, 1, 5)
-            
-            new_row["rating"] = np.round(new_rating, 2)
+            if np.random.rand() < 0.7:
+                price_update = np.random.normal(0, 0.05)
+                price = np.round(price * (1 + price_update), 2)  # 🔑 update directo
+                new_row["price"] = price
+            else:
+                delta = np.random.normal(0, 0.05)
+                rating = np.round(np.clip(rating + delta, 1, 5), 2)  # 🔑 update directo
+                new_row["rating"] = rating
 
-        rows[-1]["is_current"] = False
-
-        new_row["is_current"] = True
-
-        rows.append(new_row.to_dict())
+            last_date = change_date
+            new_row["updated_at"] = change_date
+            rows.append(new_row.to_dict())
         
 scd_df = pd.DataFrame(rows)
-scd_df.to_csv("data/processed/products_processed.csv", index=False)
+scd_df.to_csv("data/processed/products_processed.csv", index=False, date_format="%Y-%m-%dT%H:%M:%S.%f")
 
 
--- bootstrap/users_tf.py
+++ bootstrap/users_tf.py
@@ -8,10 +8,9 @@ df = pd.read_csv("data/ecommerce_dataset/users.csv")
 df["signup_date"] = pd.to_datetime(df["signup_date"])
 
 df["updated_at"] = df["signup_date"]
-df["updated_at"] = df["signup_date"]
 
-prob_of_change = 0.3
-max_changes = 3
+prob_of_change = 0.4
+max_changes = 4
 
 rows = []
 
@@ -19,27 +18,33 @@ for _, row in df.iterrows():
     rows.append(row.to_dict())
     
     if np.random.rand() < prob_of_change:
-        n_changes = np.random.randint(1, max_changes + 1)
-        last_date = row["signup_date"]
+        new_row = row.copy()
+        n_changes = np.random.choice(
+            [1, 2, 3, 4],
+            p=[0.6, 0.25, 0.1, 0.05]
+        )
+        
+        last_date = row["updated_at"]
+        email = row["email"]
+        city = row["city"]
         
         for _ in range(n_changes):
-            new_row = row.copy()
-            
             change_date = last_date + pd.Timedelta(
                 days=np.random.randint(10, 180),
                 seconds=np.random.randint(0, 86400),
                 microseconds=np.random.randint(0, 1_000_000)
             )
             
-            if np.random.rand() < 0.7: #greater prob of changing email
-                new_row["email"] = fake.email()
+            if np.random.rand() < 0.9: #greater prob of changing email
+                email = fake.email()
+                new_row["email"] = email
             else:
-                new_row["city"] = fake.city()
+                city = fake.city()
+                new_row["city"] = city
             
-            new_row["updated_at"] = change_date
-            
-            rows.append(new_row.to_dict())
             last_date = change_date
+            new_row["updated_at"] = change_date
+            rows.append(new_row.to_dict())
 
 scd_df = pd.DataFrame(rows)
 scd_df.to_csv("data/processed/users_processed.csv", index=False, date_format="%Y-%m-%dT%H:%M:%S.%f")
