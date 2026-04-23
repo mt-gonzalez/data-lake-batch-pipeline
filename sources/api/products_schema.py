7478a26cb131587318dc6a0a8466d07180e9a789
Matias Gonzalez <matiastomasglez@gmail.com>
2026-04-23 20:07:29 -0300

    infra prepared, tests pending

--- sources/api/products_schema.py
+++ sources/api/products_schema.py
@@ -1,10 +1,15 @@
 from pydantic import BaseModel
-
+from typing import List, Optional
+from datetime import datetime
 class ProductBase(BaseModel):
+    product_id: str
+    updated_at: datetime
     product_name: str
     category: str
     brand: str
     price: float
     rating: float
 
-# class ReviewsResponse(BaseModel):
+class ProductResponse(BaseModel):
+    data: List[ProductBase]
+    next_cursor: Optional[str]
