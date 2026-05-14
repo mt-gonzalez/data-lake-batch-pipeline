bb8d857ce97e80099a1788097992179e6db7e3df
Matias Gonzalez <matiastomasglez@gmail.com>
2026-05-14 16:52:01 -0300

    fix in users names and passwords

--- infra/init.sh
+++ infra/init.sh
@@ -8,29 +8,29 @@ mc alias set local http://minio:9000 mtgonzalez secret123
 
 echo "Creating buckets..."
 
-mc mb local/datalake-raw --ignore-existing
+mc mb local/datalake-bronze --ignore-existing
 mc mb local/datalake-silver --ignore-existing
 mc mb local/datalake-gold --ignore-existing
 
 
 echo "Applying policies..."
 
-mc admin policy create local raw-policy infra/policies/raw.json || true
-mc admin policy create local silver-policy infra/policies/silver.json || true
-mc admin policy create local gold-policy infra/policies/gold.json || true
+mc admin policy create local orchestration-policy infra/policies/orch_policy.json || true
+mc admin policy create local job-policy infra/policies/job_policy.json || true
+mc admin policy create local bi-policy infra/policies/bi_policy.json || true
 
 
 echo "Creating users..."
 
 mc admin user add local airflow airflow123 || true
-mc admin user add local pipeline pipeline123 || true
-mc admin user add local readonly readonly123 || true
+mc admin user add local jobs secret-jobs123 || true
+mc admin user add local bi-analyst bi-analyst123 || true
 
 
 echo "Attaching policies..."
 
-mc admin policy attach local raw-policy --user airflow || true
-mc admin policy attach local silver-policy --user pipeline || true
-mc admin policy attach local gold-policy --user readonly || true
+mc admin policy attach local orchestration-policy --user airflow || true
+mc admin policy attach local job-policy --user jobs || true
+mc admin policy attach local bi-policy --user bi-analyst || true
 
 echo "Completed."
