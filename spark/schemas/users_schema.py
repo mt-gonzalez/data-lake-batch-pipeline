4f22dc631b6de2526c3ecf249ec84084d671cc90
Matias Gonzalez <matiastomasglez@gmail.com>
2026-05-15 23:35:05 -0300

    fix bug in struct field definition

--- spark/schemas/users_schema.py
+++ spark/schemas/users_schema.py
@@ -4,8 +4,8 @@ users_schema = T.StructType([
     T.StructField("user_id", T.StringType(), False),
     T.StructField("name", T.StringType(), True),
     T.StructField("email", T.StringType(), True),
-    T.StructType("gender", T.StringType(), True),
-    T.StructType("city", T.StringType(), True),
-    T.StructType("signup_date", T.DateType(), True),
-    T.StructType("updated_at", T.TimestampType(), False)
+    T.StructField("gender", T.StringType(), True),
+    T.StructField("city", T.StringType(), True),
+    T.StructField("signup_date", T.DateType(), True),
+    T.StructField("updated_at", T.TimestampType(), False)
 ])
